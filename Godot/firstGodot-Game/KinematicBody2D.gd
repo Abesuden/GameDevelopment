8b2bb5681f462d4b647d338231e2803a638c9961
Alexander Besuden <43247793+Abesuden@users.noreply.github.com>
2020-06-12 12:47:53 -0400

    Updated Player Movement
    Added animations to the famer. Cleanded up some movement code.

--- Godot/firstGodot-Game/KinematicBody2D.gd
+++ Godot/firstGodot-Game/KinematicBody2D.gd
@@ -1,25 +1,41 @@
 extends KinematicBody2D
 
 # GLOBAL VARIABLES
-export var acceleration = 1000
-export var max_speed = 10000
-export var friction = 1000
+export var acceleration = 1200
+export var max_speed = 400
+export var friction = 1200
+export var prioritize_X_axis = true
+var vertical_priority = 1
 
 var velocity = Vector2.ZERO
 
-# PLAYER MOVEMENT
+# ANIMATE PLAYER
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+# DETERMIN CONTROL AXIS PRIORITY
+func _ready():
+	if (prioritize_X_axis):
+		vertical_priority = 0.9
+	else:
+		vertical_priority = 1.1
+
+# PLAYER MOVEMENT AND ANIMATION
 func _physics_process(delta):
 
 	# GET VELOCITY
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) * vertical_priority
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * acceleration * delta
-		velocity = velocity.clamped(max_speed * delta)
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
 	# ACTION
